8a0c5da260613089f48207581ee4d7b8ce1845fa
saippuakauppias <progr-m@yandex.ru>
2011-10-08 01:27:42 +0400

    small update setup, add manifest

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author = 'Vladimir Garvardt',
     author_email = 'vgarvardt@gmail.com',
     description = 'Django application for Loginza service',
+    url = 'https://github.com/vgarvardt/django-loginza',
     packages = find_packages(exclude=[
         'test_project', 'test_project.*', 'fabfile']),
     include_package_data = True,
