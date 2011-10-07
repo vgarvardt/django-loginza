fb21012115ba5b3e8de221e447b1630dcbaddd83
saippuakauppias <progr-m@yandex.ru>
2011-10-07 21:26:44 +0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'django-loginza',
+    version = '0.1',
+    author = 'Vladimir Garvardt',
+    author_email = 'vgarvardt@gmail.com',
+    description = 'Django application for Loginza service',
+    packages = find_packages(exclude=[
+        'test_project', 'test_project.*', 'fabfile']),
+    include_package_data = True,
+    zip_safe = False,
+)
