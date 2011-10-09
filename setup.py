adcee674df52d53cab615e09e76ca7d3714b6e20
saippuakauppias <progr-m@yandex.ru>
2011-10-09 23:29:39 +0400

    убрал fabfile из setup

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(
     author_email = 'vgarvardt@gmail.com',
     description = 'Django application for Loginza service',
     url = 'https://github.com/vgarvardt/django-loginza',
-    packages = find_packages(exclude=[
-        'test_project', 'test_project.*', 'fabfile']),
+    packages = find_packages(exclude=['test_project', 'test_project.*']),
     include_package_data = True,
     zip_safe = False,
     requires= [ 'Django' ],
