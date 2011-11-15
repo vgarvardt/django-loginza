88f11969d917ae0a3366de39f8201b9a11caa08d
Vladimir Garvardt <vgarvardt@gmail.com>
2011-11-16 00:20:07 +0400

    added license files and classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-loginza',
-    version = '0.3.3',
+    version = '0.3.4',
     author = 'Vladimir Garvardt',
     author_email = 'vgarvardt@gmail.com',
     description = 'Django application for Loginza service',
@@ -10,5 +10,14 @@ setup(
     packages = find_packages(exclude=['test_project', 'test_project.*']),
     include_package_data = True,
     zip_safe = False,
-    install_requires = ['Django']
+    install_requires = ['Django'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
 )
