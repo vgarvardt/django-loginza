0a9e97c4560e0127b6d930840e1c5dd46a69050d
Vladimir Garvardt <vgarvardt@gmail.com>
2012-12-09 00:19:54 +0400

    bump version to 0.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-loginza',
-    version = '0.3.6',
+    version = '0.3.7',
     author = 'Vladimir Garvardt',
     author_email = 'vgarvardt@gmail.com',
     description = 'Django application for Loginza service',
